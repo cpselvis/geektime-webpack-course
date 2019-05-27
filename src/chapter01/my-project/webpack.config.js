fa2a5029261a0c718b03e6ac196a7669803324f0
cpselvis <cpselvis@gmail.com>
2019-05-27 08:10:38 +0800

    feat: 添加第一章代码

---
+++ src/chapter01/my-project/webpack.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'production'
+};
